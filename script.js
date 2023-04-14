76899478dbb43eb8eb765c0a0f1416a98ecf1818
julia guarnieri <juliaguarnieribs@gmail.com>
2023-04-14 13:31:57 -0300

    adição do form

--- script.js
+++ script.js
@@ -34,3 +34,32 @@ btn_gen.addEventListener('click', ()=> {
         text: "Java, MySQL, Spring Framework, Testes Unitários, Git e GitHub, SCRUM, BootsTrap, Node.js, TypeScript."
     });
 });
+
+// ENVIO DE FORMULÁRIO
+
+const form = document.querySelector('#contact-form');
+const successMessage = document.querySelector('#success-message');
+const errorMessage = document.querySelector('#error-message');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  
+  const data = new FormData(form);
+  
+  try {
+    await fetch('https://formspree.io/f/xrgvojkb', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      },
+      body: data
+    });
+    
+    form.reset();
+    successMessage.style.display = 'block';
+  } catch (error) {
+    errorMessage.style.display = 'block';
+  }
+});
+
+  
